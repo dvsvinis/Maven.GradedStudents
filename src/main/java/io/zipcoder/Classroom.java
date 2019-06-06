57ff8551c272b4cb131f926a8180880de3a18d40
donnasvinis <dvsvinis@mac.com>
2019-06-06 07:46:05 -0400

    Working on Classroom Tests

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,68 @@
 package io.zipcoder;
 
+import java.util.*;
+import java.util.Collections;
+
 public class Classroom {
+    private Student[] students;
+
+    public Classroom(Student[] students) {
+        this.students = students;
+    }
+
+    public Classroom (){
+        this.students = new Student[30];
+    }
+
+    public Classroom (int maxStudents){
+        this.students = new Student[maxStudents];
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public Double getAverageExamScore(){
+        Double sum = 0.0;
+        for(Student each : students)
+            sum += each.getAverageExamScore();
+        return sum / students.length;
+    }
+
+    public void addStudent(Student student){
+        for (int i = 0; i < students.length; i++){
+            if (students[i] == null) {
+                students[i] = student;
+                break;
+            }
+        }
+    }
+
+    public void removeStudent(String firstName, String lastName){
+        List<Student> arrayList = new ArrayList<Student>(Arrays.asList(students));
+        int classSize = students.length;
+        for (Student each : students){
+            if (each.getFirstName() == firstName && each.getLastName() == lastName) {
+                arrayList.remove(each);
+            }
+        }
+        students = arrayList.toArray(new Student[classSize]);
+    }
+
+    public Student[] getStudentsByScore(){
+
+        List<Student> sortedByScore = new ArrayList<Student>(Arrays.asList(students));
+        Collections.sort(sortedByScore, new Comparator<Student>() {
+            public int compare(Student s1, Student s2) {
+                return Double.valueOf(s1.getAverageExamScore()).compareTo(s2.getAverageExamScore());
+            }
+        });
+
+        Student[] output = new Student[sortedByScore.size()];
+        return sortedByScore.toArray(output);
+    }
+
+
 }
+
+
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,52 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class ClassroomTest {
+    @Test
+    public void getStudents() {
+        ArrayList<Double> s1Scores = new ArrayList<Double>();
+        s1Scores.add(90.0);
+        s1Scores.add(91.0);
+        ArrayList<Double> s2Scores = new ArrayList<Double>();
+        s2Scores.add(70.0);
+        s2Scores.add(75.0);
+
+        Student s1 = new Student("student","one", s1Scores);
+        Student s2 = new Student("student","two", s2Scores);
+        Student[] students = new Student[]{s1, s2};
+        Classroom classroom = new Classroom(students);
+
+        for (Student each : students)
+        System.out.println(each.toString());
+    }
+
+    @Test
+    public void getAverageExamScore() {
+    }
+
+    @Test
+    public void addStudent() {
+ //       Double[] s1Scores = { 100.0, 150.0 }
+ //       Double s2Scores = { 225.0, 25.0 }
+//        Student s1 = new Student("student","one", s1Scores);
+//        Student s2 = new Student("student","two", s2Scores);
+//
+//        Student[] students = new Student{s1,s2};
+//        Classroom classroom = new Classroom(students);
+
+ //       String expected = "Leon";
+ //       Assert.assertEquals(expected, student.getFirstName());
+    }
+
+    @Test
+    public void removeStudent() {
+    }
+
+    @Test
+    public void getStudentsByScore() {
+    }
 }
